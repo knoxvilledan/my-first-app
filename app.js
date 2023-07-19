1a33d09b503a07aa3a9aff53f0439543e787a16b
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-07-18 21:01:38 -0400

    test

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database connected!");
+  db.close();
+});
